da92607d4e509ca216b3a26087832f9a17ffcd6f
Shiaondo Orkuma <shiaondoorkuma@gmail.com>
2020-04-20 20:24:43 +0100

    Updating my app with a collection preview component and a collection item component

--- src/Componets/MenuItems/MenuItems.js
+++ src/Componets/MenuItems/MenuItems.js
@@ -13,8 +13,7 @@ const MenuItems = ({ title, imageUrl, size, history, linkUrl, match }) => (
     />
 
     <div className="content">
-      <h1 className="titel">{title.toUpperCase()}</h1>
-
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitile">SHOP NOW</span>
     </div>
   </div>
